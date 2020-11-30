4e650b044fb78af7b39e95b8c8dcff8068b6c05b
Trongdev <hiiamtrong@gmail.com>
2020-11-30 17:45:46 +0700

    update

--- scripts/pythons/querymongo.py
+++ scripts/pythons/querymongo.py
@@ -4,7 +4,8 @@ import sys
 myclient = pymongo.MongoClient("mongodb://localhost:27017/")
 mydb = myclient["4handy-work-dev"]
 mycol = mydb["usertagentries"]
-mydocs = mycol.find().sort("created").limit(int(sys.argv[1]))
+limit = int(sys.argv[1])
+mydocs = mycol.find({}).sort("created", pymongo.DESCENDING).limit(limit)
 result = []
 for doc in mydocs:
     need = {"status": doc['status'], "taggedUsers": [{'user': taggedUser['user']} for taggedUser in doc['taggedUsers']],
