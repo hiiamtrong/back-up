b274be0c8e82302c067adab909e2f2de3798eb79
Trongdev <hiiamtrong@gmail.com>
2020-12-02 17:34:31 +0700

    update

--- scripts/pythons/querymongo.py
+++ scripts/pythons/querymongo.py
@@ -2,6 +2,7 @@ import pymongo
 import sys
 import os
 env = sys.argv[2]
+collection = sys.argv[3]
 mongo_url = os.getenv('MONGO_URL')
 myclient = pymongo.MongoClient(mongo_url)
 mydb = myclient["4handy-work"]
@@ -11,13 +12,13 @@ if env == 'dev':
     print('dev')
 else:
     print('prod')
-mycol = mydb["usertagentries"]
+mycol = mydb[collection]
 limit = int(sys.argv[1])
 mydocs = mycol.find({}).sort("created", pymongo.DESCENDING).limit(limit)
 result = []
 for doc in mydocs:
-    need = {"status": doc['status'], "taggedUsers": [{'user': taggedUser['user']} for taggedUser in doc['taggedUsers']],
-            "notiId": doc['notiId'], "description": doc['description']}
-    result.append(need)
+    # need = {"status": doc['status'], "taggedUsers": [{'user': taggedUser['user']} for taggedUser in doc['taggedUsers']],
+    #         "notiId": doc['notiId'], "description": doc['description']}
+    result.append(doc)
 
 print((result))
