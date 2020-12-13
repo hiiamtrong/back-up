9fcc4c1ef63eaf03ae667d0d14d243eb66aadb25
Trongdev <hiiamtrong@gmail.com>
2020-12-14 01:42:54 +0700

    update

--- scripts/pythons/sheet.py
+++ scripts/pythons/sheet.py
@@ -1,13 +1,16 @@
 from __future__ import print_function
+from google.auth.transport.requests import Request
+from google_auth_oauthlib.flow import InstalledAppFlow
+from googleapiclient.discovery import build
+import os.path
 from datetime import date
 import json
 import pydash as _
 from json import loads, dumps
 import pickle
-import os.path
-from googleapiclient.discovery import build
-from google_auth_oauthlib.flow import InstalledAppFlow
-from google.auth.transport.requests import Request
+current_path = os.path.dirname(os.path.abspath(__file__))
+credentials_path = f'{current_path}/credentials.json'
+
 # path = open('./output.json')
 # data = loads(path.read())
 # If modifying these scopes, delete the file token.pickle.
@@ -25,8 +28,8 @@ def init(data):
     # The file token.pickle stores the user's access and refresh tokens, and is
     # created automatically when the authorization flow completes for the first
     # time.
-    if os.path.exists('token.pickle'):
-        with open('token.pickle', 'rb') as token:
+    if os.path.exists(f'{current_path}/token.pickle'):
+        with open(f'{current_path}/token.pickle', 'rb') as token:
             creds = pickle.load(token)
     # If there are no (valid) credentials available, let the user log in.
     if not creds or not creds.valid:
@@ -34,10 +37,10 @@ def init(data):
             creds.refresh(Request())
         else:
             flow = InstalledAppFlow.from_client_secrets_file(
-                'credentials.json', SCOPES)
+                credentials_path, SCOPES)
             creds = flow.run_local_server(port=0)
         # Save the credentials for the next run
-        with open('token.pickle', 'wb') as token:
+        with open(f'{current_path}/token.pickle', 'wb') as token:
             pickle.dump(creds, token)
 
     service = build('sheets', 'v4', credentials=creds)
