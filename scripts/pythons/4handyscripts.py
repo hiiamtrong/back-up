b2135668f5d2ea482000ac96aea722539a8642aa
trongdev <hiiamtrong@gmail.com>
2021-01-19 18:08:48 +0700

    Sửa-lại-export-sheet

--- scripts/pythons/4handyscripts.py
+++ scripts/pythons/4handyscripts.py
@@ -1,4 +1,3 @@
-
 import json
 import calcpoint
 import requests
@@ -8,54 +7,63 @@ import moment
 import pyperclip
 import sheet
 import os.path
+
 current_path = os.path.dirname(os.path.abspath(__file__))
 
 
 lists = {
     "5e5e1391e3c0053b8a4ecadd": ":pencil: *Todo*",
     "5958d058ce48d6ee6e437912": ":bulb: *Doing*",
-    "5f8926abf5f741310a8c695d": ":wrench: *Draft*"
+    "5f8926abf5f741310a8c695d": ":wrench: *Draft*",
 }
 
 
 def get_task_gitlab():
     url_gitlab = f'https://gitlab.com/api/v4/projects/{gitlab["PROJECT_ID"]}/merge_requests?author_username={gitlab["USER_GITLAB"]}&private_token={gitlab["ACCESS_TOKEN_GITLAB"]}&state={gitlab["STATE"]}'
     tasks = requests.get(url_gitlab).json()
-    tasks_text = ''
+    tasks_text = ""
     for task in tasks:
-        approve_status = f' :white_check_mark: *{task["merge_status"]}*' if task[
-            'merge_status'] != 'unchecked' else f' :warning: *{task["merge_status"]}*'
-        if not task['work_in_progress']:
+        approve_status = (
+            f' :white_check_mark: *{task["merge_status"]}*'
+            if task["merge_status"] != "unchecked"
+            else f' :warning: *{task["merge_status"]}*'
+        )
+        if not task["work_in_progress"]:
             tasks_text += f'- [{approve_status}] [{task.get("title")}]({task.get("web_url")}) update gần nhất: **{moment.date(task.get("updated_at")).format("YYYY-M-D h:m A")}** - {task["user_notes_count"]} comments\n'
     return tasks_text
 
 
 def get_task_trello():
-    path_cards = open(f'{current_path}/cards-trello.json')
+    path_cards = open(f"{current_path}/cards-trello.json")
     cards = json.loads(path_cards.read())
-    cards_text = ''
+    cards_text = ""
     for card in cards:
-        if trello['USER_ID'] in card.get('idMembers', []) and card.get('idList') in ['5e5e1391e3c0053b8a4ecadd', '5958d058ce48d6ee6e437912', '5f8926abf5f741310a8c695d']:
-            due = 'Chưa có'
-            status = lists.get(card['idList'])
-            if card.get('due', ''):
-                due = moment.date(card.get('due')).format(
-                    'YYYY-M-D h: m A')
-            cards_text += f"- [{status}] [{card['name']}]({card['shortUrl']}) due: **{due}**\n"
+        if trello["USER_ID"] in card.get("idMembers", []) and card.get("idList") in [
+            "5e5e1391e3c0053b8a4ecadd",
+            "5958d058ce48d6ee6e437912",
+            "5f8926abf5f741310a8c695d",
+        ]:
+            due = "Chưa có"
+            status = lists.get(card["idList"])
+            if card.get("due", ""):
+                due = moment.date(card.get("due")).format("YYYY-M-D h: m A")
+            cards_text += (
+                f"- [{status}] [{card['name']}]({card['shortUrl']}) due: **{due}**\n"
+            )
     return cards_text
 
 
 def option_summary_point():
-    path_labels = open(f'{current_path}/labels-trello.json')
+    path_labels = open(f"{current_path}/labels-trello.json")
     labels = json.loads(path_labels.read())
     labels_map = {}
     for index, label in enumerate(labels):
         print(f"{index}. {label['name']}\n")
     for index, label in enumerate(labels):
-        labels_map[f'{index}'] = label['name']
-    label = input('Nhập label: ')
+        labels_map[f"{index}"] = label["name"]
+    label = input("Nhập label: ")
     result = calcpoint.calc_point_trello(trello, labels_map[label])
-    path_test = open(f'{current_path}/output.json', 'w')
+    path_test = open(f"{current_path}/output.json", "w")
     path_test.write(json.dumps(result))
     sheet.init(result)
     return False
@@ -70,13 +78,16 @@ def options(option):
         "1": checkin,
         "2": option_summary_point,
     }
-    return switcher.get(option, f'=======Doing=======\n{get_task_trello()}\n========QC========\n{get_task_gitlab() if get_task_gitlab() else "Không có"}')()
+    return switcher.get(
+        option,
+        f'=======Doing=======\n{get_task_trello()}\n========QC========\n{get_task_gitlab() if get_task_gitlab() else "Không có"}',
+    )()
 
 
 def main():
-    print('1. Checkin')
-    print('2. Summary Point')
-    option = input('Chọn option: ')
+    print("1. Checkin")
+    print("2. Summary Point")
+    option = input("Chọn option: ")
     result = options(option)
     if result:
         print(result)
--- scripts/pythons/calcpoint.py
+++ scripts/pythons/calcpoint.py
@@ -5,15 +5,16 @@ import requests
 import asyncio
 import aiohttp
 import os.path
+
 current_path = os.path.dirname(os.path.abspath(__file__))
 
 # initial
 callapi.init()
-path_cards = open(f'{current_path}/cards-trello.json')
-path_custom_fields = open(f'{current_path}/customFields-trello.json')
-path_custom_members = open(f'{current_path}/members-trello.json')
-path_lists = open(f'{current_path}/lists-trello.json')
-path_labels = open(f'{current_path}/labels-trello.json')
+path_cards = open(f"{current_path}/cards-trello.json")
+path_custom_fields = open(f"{current_path}/customFields-trello.json")
+path_custom_members = open(f"{current_path}/members-trello.json")
+path_lists = open(f"{current_path}/lists-trello.json")
+path_labels = open(f"{current_path}/labels-trello.json")
 cards = json.loads(path_cards.read())
 custom_fields = json.loads(path_custom_fields.read())
 members = json.loads(path_custom_members.read())
@@ -23,11 +24,11 @@ members_map = {}
 custom_fields_map = {}
 lists_map = {}
 labels_map = {}
-summary_cards = {}
+summary_cards = []
 for member in members:
-    members_map[member['id']] = member['fullName']
+    members_map[member["id"]] = member["fullName"]
 for field in custom_fields:
-    custom_fields_map[field['id']] = field['name']
+    custom_fields_map[field["id"]] = field["name"]
 need_lists = [
     "5e5e1391e3c0053b8a4ecadd",
     "5958d058ce48d6ee6e437912",
@@ -36,29 +37,46 @@ need_lists = [
     "5958d06a118336d803b09183",
 ]
 for list in lists:
-    if(list['id'] in need_lists):
-        lists_map[list['id']] = list['name']
+    if list["id"] in need_lists:
+        lists_map[list["id"]] = list["name"]
 for label in labels:
-    labels_map[label['id']] = label['name']
+    labels_map[label["id"]] = label["name"]
 
 
 def calc_point_trello(trello_credential, label):
     existsLabel = _.find(labels_map, lambda _label: _label == label)
     if not existsLabel:
         raise Exception(f"Label {label} không tồn tại")
-    cards_with_points = _.filter_(asyncio.get_event_loop().run_until_complete(
-        get_all_point(cards, trello_credential)))
     cards_with_points = _.filter_(
-        cards_with_points, lambda card: label in _.map_(card['labels'], 'name'))
+        asyncio.get_event_loop().run_until_complete(
+            get_all_point(cards, trello_credential)
+        )
+    )
+    cards_with_points = _.filter_(
+        cards_with_points, lambda card: label in _.map_(card["labels"], "name")
+    )
     for card in cards_with_points:
-        for member in card['idMembers']:
-            if(not summary_cards.get(member)):
-                summary_cards[member] = []
-            summary_cards[member].append(
-                {"name": card["name"], "id": card["id"], "label": _.map_(card['labels'], 'name'), "point": card["point"], "list": lists_map[card["idList"]], "listId": card["idList"]}, )
+        useful_info = {
+            "name": card["name"],
+            "id": card["id"],
+            "label": _.map_(card["labels"], "name"),
+            "point": card["point"],
+            "list": lists_map[card["idList"]],
+            "listId": card["idList"],
+            "link": "https://trello.com/c/" + card["shortLink"],
+        }
+        if not len(card["idMembers"]):
+            summary_cards.append(useful_info)
+        else:
+            for member in card["idMembers"]:
+                useful_info["username"] = members_map[member]
+                useful_info["userId"] = member
+                summary_cards.append(useful_info)
+
     return {
-        "users": summary_by_users(summary_cards),
-        "lists": summary_by_lists(cards_with_points)
+        "cards": summary_cards,
+        "users": get_uniq_users(summary_cards),
+        "lists": get_uniq_lists(summary_cards),
     }
 
 
@@ -66,74 +84,48 @@ async def get_all_point(cards, trello_credential):
     async with aiohttp.ClientSession() as session:
         points = []
         for card in cards:
-            point = asyncio.create_task(
-                get_point(card, session, trello_credential))
+            point = asyncio.create_task(get_point(card, session, trello_credential))
             points.append(point)
         cards_with_points = await asyncio.gather(*points, return_exceptions=True)
         return cards_with_points
 
 
 async def get_point(card, session, trello_credential):
-    if(card['idList'] not in lists_map.keys()):
+    if card["idList"] not in lists_map.keys():
         return
     url_trello = f'https://api.trello.com/1/cards/{card["id"]}/customFieldItems'
-    headers = {
-        "Accept": "application/json"
-    }
+    headers = {"Accept": "application/json"}
     query = {
-        'token': f'{trello_credential["ACCESS_TOKEN_TRELLO"]}',
-        'key': f'{trello_credential["KEY"]}',
+        "token": f'{trello_credential["ACCESS_TOKEN_TRELLO"]}',
+        "key": f'{trello_credential["KEY"]}',
     }
-    async with session.get(url_trello, headers=headers, params=query,) as response:
-        if(response.status != 200):
+    async with session.get(
+        url_trello,
+        headers=headers,
+        params=query,
+    ) as response:
+        if response.status != 200:
             return
         response = await response.json()
-        custom_field = _.find(
-            response, {'idCustomField': '5f8e5b92e141341c11fe1b1d'})
+        custom_field = _.find(response, {"idCustomField": "5f8e5b92e141341c11fe1b1d"})
         point = 0
-        if(custom_field):
-            point = custom_field['value']['number']
-        card['point'] = point
+        if custom_field:
+            point = custom_field["value"]["number"]
+        card["point"] = point
         return card
 
 
-def summary_by_users(summary_cards):
+def get_uniq_users(summary_cards):
     users = {}
-    for user in summary_cards:
-        user_name = members_map[user]
-        total_point = sum(int(card["point"]) for card in summary_cards[user])
-        lists_map_clones = {}
-        for list in lists:
-            if(list['id'] in need_lists):
-                lists_map_clones[list['name']] = {
-                    "cards": [],
-                    "list-points": 0
-                }
-        if(not users.get(user_name)):
-            users[user_name] = {"lists": lists_map_clones,
-                                "total_point": total_point}
-        for card in summary_cards[user]:
-            users[user_name]["lists"][card["list"]]["cards"].append(card)
-            users[user_name]["lists"][card["list"]
-                                      ]["list-points"] += int(card.get('point', 0))
+    for card in summary_cards:
+        if not card.get("userId"):
+            continue
+        users[card.get("userId")] = card["username"]
     return users
 
 
-def summary_by_lists(cards):
+def get_uniq_lists(summary_cards):
     _lists = {}
-    for list in lists:
-        if(list['id'] in need_lists):
-            _lists[list['name']] = {
-                "cards": [],
-                "total_points": 0
-            }
-    cards_group_by_lists = _.group_by(cards, 'idList')
-    for list in cards_group_by_lists:
-        list_name = lists_map[list]
-        total_points = sum(int(card["point"])
-                           for card in cards_group_by_lists[list])
-        _lists[list_name]['total_points'] = total_points
-        for card in cards_group_by_lists[list]:
-            _lists[list_name]["cards"].append({"name": card["name"], "id": card["id"], "label": _.map_(
-                card['labels'], 'name'), "point": card["point"], "list": lists_map[card["idList"]], "listId": card["idList"]},)
+    for card in summary_cards:
+        _lists[card["listId"]] = card["list"]
     return _lists
